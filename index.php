b21b6523a433d91da8bb54794bdbb2b54c5a9c0d
Gabriel Vitor <git624886@gmail.com>
2023-04-04 10:19:04 -0300

    feat: sendResponse

--- index.php
+++ index.php
@@ -5,4 +5,5 @@ require_once __DIR__ . '/vendor/autoload.php';
 use HttpClasses\Router;
 
 $routerInstance = Router::init(__DIR__ . '/routes');
-$routerInstance->run();
+$routerInstance->run()
+  ->sendResponse();
